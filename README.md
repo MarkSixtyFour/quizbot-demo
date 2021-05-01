cb1db0b408757c6fca72e8cf3745792e99fd0370
62470975+cvui@users.noreply.github.com <62470975+mklxiv@users.noreply.github.com>
2021-05-01 16:35:56 -0400

    Initial "playable" state

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # quizbot-demo
 A simple Discord quiz/trivia bot created as an assignment for a class, CIT412 - Computer Simulation Applications. Note that this bot isn't yet feature-complete and is rather early-on in development. I haven't made such a program before and I wanted to give it a shot, my class project was a good opportunity.
 
+## A fair warning
+Please note that the code is probably fairly broken and spaghetti code at this point, but that will be resolved when this is no longer a class assignment with many other assignments due and finals coming up soon (aka when I'm not in a hurry).
+
 ## What does it do so far?
-So far, the bot can get questions at random from JSON files and respond to a few test commands. It doesn't yet validate the JSON files so be careful when making new questions for the bot to read.
+So far, the bot can get questions at random from JSON files, shuffle the answers so the order isn't the same each time, and respond to a answers to a question. It doesn't yet validate the JSON files so be careful when making new questions for the bot to read. Questions should have at least 2 responses.
 
 ## Required setup
 This bot must be self-hosted and doesn't include an API key. To run this bot, ensure you have NPM installed, along with all dependencies of the package. Make a new JSON file called `token.json` and put it in the bot's root directory, and ensure it has the following contents:
@@ -17,12 +20,19 @@ Then, run the bot using `node`:
 node bot.js
 ```
 
+You can test questions by typing the following into the Discord chat where it resides:
+```
+./test-msg
+```
+
+When you're given a question, react to the generated message with one of the emojis it indicates for a specific answer. It should respond with if you were correct or incorrect.
+
 ## To-do list
 - [x] Get the bot connecting to Discord, and responding to basic commands
 - [x] Randomly select questions from JSON
-- [ ] Use message reactions to read user responses
-- [ ] Create additional test commands to test base functionality
+- [x] Use message reactions to read user responses
 - [ ] Validate JSON files at run-time and alert users of incorrect JSON files
 - [ ] Use embeds to make output prettier and more useful
 - [ ] Implement base command set to play the game properly
 - [ ] Store user scores
+- [ ] Write a tool to generate question JSONs in a user-friendly manner
--- bot.js
+++ bot.js
@@ -1,22 +1,27 @@
-// Dependencies
+/* Dependencies */
 const Discord = require('discord.js');
 const path = require('path');
 const fs = require('fs');
 
-// Configuration files
+/* Configuration files */
 const token = require(path.join(__dirname, 'token.json'));
 const config = require(path.join(__dirname, 'config.json'));
 
-// Setup
+/* Setup */
 const questionDir = path.join(__dirname, 'questions');
+const responseReactions = ['🇦', '🇧', '🇨', '🇩', '🇪'];
+const maxRespnses = responseReactions.length;
 const client = new Discord.Client();
 
 // Utility functions
+
+/* Random number in range */
 function randRange(min, max) {
     const num = Math.random() * (max - min) + min;
     return Math.floor(num);
 }
 
+/* Fetch a random question */
 function getRandomQuestion() {
     let randomFiles = fs.readdirSync(questionDir);
     const randomFileName = randomFiles[randRange(0, randomFiles.length)];
@@ -24,24 +29,84 @@ function getRandomQuestion() {
     return JSON.parse(questionFile);
 }
 
+/* Fisher-Yates shuffle - https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm */
+function shuffle(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        let j = randRange(0, i);
+        [arr[j], arr[i]] = [arr[i], arr[j]];
+    }
+}
+/* Format a question for being sent as a message */
+function questionFormat(arr) {
+    let builtString = ``;
+    let upTo = arr.length <= maxRespnses ? arr.length : maxRespnses;
+
+    if (arr.length == 0) return constructedString;
+
+    for (let i = 0; i < upTo; i++) {
+        builtString = builtString + responseReactions[i] + ' ' + arr[i] + '\n';
+    }
+
+    return builtString;
+}
+
+// On startup
+
 client.once('ready', () => {
     console.log('Bot has started');
 });
 
-// Bot commands
+// Command handling
+
 client.on('message', (message) => {
     if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-    const args = message.content.slice(config.prefix.length).trim().split(' ');
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
 
     if (command === 'start') {
-        message.channel.send('Quiz has started!');
+        start(message.channel);
     } else if (command === 'stop') {
-        message.channel.send('Quiz has ended!');
+        stop(message.channel);
     } else if (command === 'test-msg') {
-        var randQuestion = getRandomQuestion();
-        message.channel.send(randQuestion.question);
+        let randQ = getRandomQuestion();    /* Get a random question */
+        let upTo = randQ.responses.length <= maxRespnses ? randQ.responses.length : maxRespnses;
+        let correctReact = null;            /* Used for determining the correct reaction */
+        let allReacts = [];                 /* All possible reactions for this question */
+
+        if (upTo < 2) return;               /* This is a quiz, there needs to be at least 2 responses */
+
+        shuffle(randQ.responses);           /* Randomize answer order */
+
+        for (let i = 0; i < upTo; i++) {
+            allReacts.push(responseReactions[i]);
+            if (randQ.responses[i] == randQ.answer)
+                correctReact = responseReactions[i];
+        }
+
+        message.channel.send('Question: ' + randQ.question + '\n'
+                  + 'Value: ' + randQ.value + '\n\n'
+                  + questionFormat(randQ.responses) + '\n'
+                  + '_React within **' + config.questionTime / 1000 + '  seconds** with your answer!_')
+        .then(async msg => {
+            const filter = (reaction, user) => {
+                return allReacts.includes(reaction.emoji.name) && user.id == message.author.id;
+            };
+            
+            await msg.awaitReactions(filter, {max: 1, time: config.questionTime, errors: ['time']})
+                .then(collected => {
+                    const reaction = collected.first();
+                    
+                    if (reaction.emoji.name == correctReact)
+                        message.reply("correct!");
+                    else
+                        message.reply("incorrect!");
+                    })
+                    .catch(collected => {
+                        message.reply("no response received!");
+                    })
+        })
+        .catch();
     }
 });
 
